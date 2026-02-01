a9195509fd26599149595a02af3a4790dfd7d3a3
Abhishek Singh <abhishekongit@gmail.com>
2026-02-01 10:34:20 +0530

    meshes

--- 9_manipulating_meshes/manipulating_meshes_starter/src/script.js
+++ 9_manipulating_meshes/manipulating_meshes_starter/src/script.js
@@ -6,17 +6,25 @@ const scene = new THREE.Scene();
 
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
 scene.add(cubeMesh);
+
+cubeMesh.rotation.reorder("YXZ");
+cubeMesh.rotation.y= THREE.MathUtils.degToRad(45);
+cubeMesh.rotation.x= THREE.MathUtils.degToRad(45);
+
+
+const axesHelper = new THREE.AxesHelper(2);
+cubeMesh.add(axesHelper);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
   35,
   window.innerWidth / window.innerHeight,
   0.1,
-  200
+  200,
 );
 camera.position.z = 5;
 
@@ -27,22 +35,22 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // instantiate the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 // controls.autoRotate = true;
 
-window.addEventListener('resize', () =>{
+window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix()
+  camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-})
+});
 
 // render the scene
 const renderloop = () => {
-  controls.update();  
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
 };
